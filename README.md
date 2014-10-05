9455bd1bfc3c9299701bc1c458b5724e8748e59f
Mexico Evalua <mexicoevaluadesarrollo@gmail.com>
2014-10-05 15:58:52 -0400

    Primer commit

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-ingresos
+Ingresos del sector público presupuestario 1994-2013
 ========
 
-Treemap para los ingresos del sector público en México 1994-2013
+Cálculos para crear un treemap con los ingresos del sector público 1994-2013.  
+Los datos fueron procesados con R y la visualziación hecha con Tableau Public.  
+Fuente: [Segundo Informe de Gobierno del Presidente Enrique Peña Nieto](http://www.presidencia.gob.mx/segundoinforme/). Anéxo estadístico, páginas 383 y 384.
+![treemap ingreso](http://gasto.iniciativasgo.com/wp-content/uploads/2014/10/postImage.png)
---
+++ ingresos.R
@@ -0,0 +1,96 @@
+# Treemaps para el ingresos del gob federal de 1994-2015
+# El script organiza los datos y la visualización se hizo con Tableau Public
+
+# Directorio de trabajo
+setwd("D:/DATA.IDB/Documents/ME/PEF")
+setwd("~/Dropbox/Consultoría/micrositios/ingresos")
+
+# Cargar datos armonizados que contienen dos series para 96, 97 y 00
+data  <- read.csv("ingresos_1994-2015.csv", encoding="UTF8",
+                  stringsAsFactors=F)
+str(data)
+names(data)
+head(data)
+
+# Cambiar formato de los datos a long
+require(reshape2)
+data  <- melt(data, id.vars= c("year","total"),
+              variable.name="concepto", value.name="mdp")
+head(data)
+
+data$pctTot  <- with(data, (mdp / total) * 100)
+head(data)
+str(data$pctTot)
+
+# Cargar datos PIB
+source("gdp.R")
+head(gdp)
+gdp  <- gdp[,-3]
+
+# Unir los dos archivos
+ing  <- merge(data, gdp)
+head(ing)
+
+# Calcular gasto por ramo como % del pib
+ing$conPctPib <- with(ing, (mdp/gdpCorriente)*100)
+ing$totIngPctPib  <- with(ing, (total/gdpCorriente)*100)
+head(ing)
+
+# Eliminar variables
+names(ing)
+ing  <- ing[,-6]
+head(ing)
+
+# Agregar categorias
+###################################
+require(plyr)
+unique(ing$concepto)
+table(ing$concepto)
+ing$tipo <- ing$concepto
+unique(ing$tipo)
+
+# Petroleros
+petroleros  <- c("iepsGasDie","derechos","aprovechamientos","otrPet","pemex")
+
+table(ing$tipo)
+
+for(i in petroleros){
+  ing$tipo  <- gsub(i,"Petroleros",ing$tipo)
+}
+table(ing$tipo)
+
+# No Petroleros
+noPetroleros  <- c("isr","ietu", "iva","ieps","importacion","ide","otros",
+                   "NoTrib","entControlDir","entDesinc","cfe","imss","issste",
+                   "entDesinc","lfc")
+for(i in noPetroleros){
+  ing$tipo  <- gsub(i,"No petroleros",ing$tipo)
+}
+table(ing$tipo)
+
+# Cambiar las etiquetas de los valores
+#####
+table(ing$concepto)
+unique(ing$concepto)
+f  <- unique(ing$concepto)
+f  <- as.character(f)
+t  <- c("IEPS de gasolina y diesel","Aprovechamientos","Total Gobierno Federal petroleros","Total petroleros", 
+        "CFE","Otros", "ISR","Derechos","Otros petroleros", "Pemex", "IEPS","IMSS","Total tributarios","IETU","ISSSTE",
+        "IDE","IVA", "Importación", "Total Gobierno Federal no petroleros", "No tributarios", "Entidades desincorporadas",
+        "LFC", "Total no petroleros", "Entidades de control directo")
+ing$concepto  <- mapvalues(x = ing$concepto,from = f, to = t)
+table(ing$concepto)
+unique(ing$concepto)
+
+# Eliminar Entidades de control directo para no duplicar cifras con CFE, LFC, etc.
+ing <- subset(ing, ing$concepto != "Entidades de control directo")
+
+# Mantener solo ing petroleros y no petroleros
+petNoPet  <- subset(ing, ing$tipo == "Petroleros" | ing$tipo =="No petroleros")
+table(petNoPet$tipo)
+head(petNoPet)
+
+# Reemplazar NAs con 0 para mantener el tamaño del rectángulo constante
+# Exportar datos como csv
+write.csv(petNoPet, "ing_pet_noPet_1994_2013.csv", fileEncoding="utf8", row.names=F,na="0")
+  
